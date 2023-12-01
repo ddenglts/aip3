770f517109885188484a5d7fce515ca2623e1551
SarthakJain2 <sarthakjain.2007@gmail.com>
2023-11-30 21:20:56 -0500

    image

---
+++ image.py
@@ -0,0 +1,87 @@
+import numpy as np
+import random
+from colorama import init, Fore, Style
+
+def generate_diagram():
+    # Initialize a 20x20 array (0 represents white color)
+    image = np.zeros((20, 20), dtype=int)
+
+    # Define colors mapping to integers
+    colors = {
+        "Red": 1,
+        "Green": 2,
+        "Blue": 3,
+        "Yellow": 4
+    }
+
+    # Randomly choose whether to start with rows or columns
+    start_with_row = random.choice([True, False])
+
+    # Choose four different colors
+    chosen_colors = random.sample(list(colors.keys()), 4)
+
+    # First selection
+    if start_with_row:
+        row = random.randint(0, 19)
+        image[row, :] = colors[chosen_colors[0]]
+    else:
+        col = random.randint(0, 19)
+        image[:, col] = colors[chosen_colors[0]]
+
+    # Second selection
+    if start_with_row:
+        col = random.randint(0, 19)
+        image[:, col] = colors[chosen_colors[1]]
+    else:
+        row = random.randint(0, 19)
+        image[row, :] = colors[chosen_colors[1]]
+
+    # Third selection
+    if start_with_row:
+        while True:
+            new_row = random.randint(0, 19)
+            if new_row != row:
+                break
+        image[new_row, :] = colors[chosen_colors[2]]
+    else:
+        while True:
+            new_col = random.randint(0, 19)
+            if new_col != col:
+                break
+        image[:, new_col] = colors[chosen_colors[2]]
+
+    # Fourth selection
+    if start_with_row:
+        while True:
+            new_col = random.randint(0, 19)
+            if new_col != col:
+                break
+        image[:, new_col] = colors[chosen_colors[3]]
+    else:
+        while True:
+            new_row = random.randint(0, 19)
+            if new_row != row:
+                break
+        image[new_row, :] = colors[chosen_colors[3]]
+
+    return image
+
+def print_diagram(image):
+    init()  # Initialize Colorama
+    for row in image:
+        for pixel in row:
+            if pixel == 0:  # White
+                print(Style.RESET_ALL + '██', end='')
+            elif pixel == 1:  # Red
+                print(Fore.RED + '██', end='')
+            elif pixel == 2:  # Green
+                print(Fore.GREEN + '██', end='')
+            elif pixel == 3:  # Blue
+                print(Fore.BLUE + '██', end='')
+            elif pixel == 4:  # Yellow
+                print(Fore.YELLOW + '██', end='')
+        print(Style.RESET_ALL)  # Reset to default after each row
+
+# Generate and print the diagram
+diagram = generate_diagram()
+print_diagram(diagram)
