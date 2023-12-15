39622b479d128f9b81149cea3659127bb223df32
David Deng <david.j.deng@rutgers.edu>
2023-12-14 19:09:40 -0500

    oh no

--- image.py
+++ image.py
@@ -10,6 +10,83 @@ returns a 20x20 array of integers representing the colors of the pixels
 4 = yellow
 '''
 
+def generate_diagram_nonlinear3(count = 1) -> tuple[np.ndarray, np.ndarray]:
+    one_hot = {
+        1: (1,0,0,0),
+        2: (0,1,0,0),
+        3: (0,0,1,0),
+        4: (0,0,0,1),
+        0: (0,0,0,0)
+    }
+
+    conved_images = []
+    dangers = []
+
+    # tuple of 2 lists, one list of images, other list of labels
+    # generate a tuple of diagrams
+    diagrams = ([], [])
+    for _ in range(count):
+        image, label = _generate_diagram()
+        diagrams[0].append(image)
+        diagrams[1].append(label)
+
+    for _,v in enumerate(zip(diagrams[0], diagrams[1])):
+        image, danger = v
+        dangers.append(danger)
+
+        # array of convoluted values from 2x2 windows with stride = 1
+        conved_image = []
+        for row in range(image.shape[0] - 1):
+            for col in range(image.shape[1] - 1):
+                e11 = image[row, col]
+                e12 = image[row, col + 1]
+                e21 = image[row + 1, col]
+                e22 = image[row + 1, col + 1]
+                conved_image.append(np.dot(one_hot[e11], one_hot[e12]) + np.dot(one_hot[e11], one_hot[e21]) + np.dot(one_hot[e11], one_hot[e22]))
+            
+        assert len(conved_image) == 19*19
+        conved_images.append(np.array(conved_image).flatten())
+
+    # combining conved_images and onehot images together into one input
+    mergeds = ([], [])
+    one_hot_diagrams = convert_diagram_hot(diagrams)
+    for i,v in enumerate(one_hot_diagrams[0]):
+        merged = []
+        merged.extend(v)
+        merged.extend(conved_images[i])
+        mergeds[0].append(merged)
+    print(one_hot_diagrams[1])
+    mergeds[1].extend(one_hot_diagrams[1].flatten())
+
+    return np.array(mergeds[0]), np.array(mergeds[1])
+
+
+        
+
+
+
+def generate_diagram_nonlinear2(count = 1) -> tuple[np.ndarray, np.ndarray]:
+    images = []
+    dangerous = []
+    for _ in range(count):
+        image, danger = _generate_diagram_2x2()
+        image = image.flatten()
+        new_image = []
+        for v in image:
+            if v == 1:
+                new_image.append([1,0,0,0])
+            elif v == 2:
+                new_image.append([0,1,0,0])
+            elif v == 3:
+                new_image.append([0,0,1,0])
+            elif v == 4:
+                new_image.append([0,0,0,1])
+
+        images.append(np.array(new_image).flatten())
+        dangerous.append(danger)
+
+    # returns tuple(array of images, array of booleans)
+    return np.array(images), np.array(dangerous)
 
 def generate_diagram_nonlinear1(count = 1) -> tuple[np.ndarray, np.ndarray]:
     images = []
@@ -35,33 +112,39 @@ def generate_diagram_nonlinear1(count = 1) -> tuple[np.ndarray, np.ndarray]:
     return np.array(images), np.array(dangerous)
 
 
-def generate_diagram_hot(count = 1) -> tuple[np.ndarray, np.ndarray]:
+def convert_diagram_hot(diagrams: tuple[np.ndarray, np.ndarray]) -> tuple[np.ndarray, np.ndarray]:
+    '''
+    diagrams need to be a tuple like:
+
+    (np.array[image1, image2, ...], np.array[label1, label2, ...])
+
+    imageN is a 2d array 
+    '''
+    # to store images to return
     images = []
     dangerous = []
-    for _ in range(count):
-        image, danger = _generate_diagram()
+
+    #iterates every image in diagrams
+    for index, v in enumerate(diagrams[0]):
+        image = v
         image = image.flatten()
         new_image = []
-        for v in image:
-            if v == 1:
-                new_image.append([1,0,0,0])
-            elif v == 2:
-                new_image.append([0,1,0,0])
-            elif v == 3:
-                new_image.append([0,0,1,0])
-            elif v == 4:
-                new_image.append([0,0,0,1])
+        for i in image:
+            if i == 1:
+                new_image.append(np.array([1,0,0,0]))
+            elif i == 2:
+                new_image.append(np.array([0,1,0,0]))
+            elif i == 3:
+                new_image.append(np.array([0,0,1,0]))
+            elif i == 4:
+                new_image.append(np.array([0,0,0,1]))
             else:
-                new_image.append([0,0,0,0])
+                new_image.append(np.array([0,0,0,0]))
 
         images.append(np.array(new_image).flatten())
-        # print(np.array(new_image).flatten()[:10])
-        # print(image)
-        dangerous.append(danger)
-
+        dangerous.append(diagrams[1][index])
     # returns tuple(array of images, array of booleans)
     return np.array(images), np.array(dangerous)
-
 
 def generate_diagram(count = 1) -> tuple[np.ndarray, np.ndarray]:
     images = []
@@ -74,6 +157,81 @@ def generate_diagram(count = 1) -> tuple[np.ndarray, np.ndarray]:
     # returns tuple(array of images, array of booleans)
     return np.array(images), np.array(dangerous)
 
+def _generate_diagram_2x2():
+    # Initialize a 20x20 array (0 represents white color)
+    image = np.zeros((2, 2), dtype=int)
+
+    # Define colors mapping to integers
+    colors = {
+        "Red": 1,
+        "Green": 2,
+        "Blue":3,
+        "Yellow": 4
+    }
+
+    # Randomly choose whether to start with rows or columns
+    start_with_row = random.choice([True, False])
+
+    # Choose four different colors
+    chosen_colors = random.sample(list(colors.keys()), 4)
+
+    red_ind = -1
+    yellow_ind = -1
+    dangerous = False
+    for i, _ in enumerate(chosen_colors):
+        if (chosen_colors[i] == "Red"):
+            red_ind = i
+        if (chosen_colors[i] == "Yellow"):
+            yellow_ind = i
+    if red_ind < yellow_ind:
+        dangerous = True
+
+    # First selection
+    if start_with_row:
+        row = random.randint(0, 1)
+        image[row, :] = colors[chosen_colors[0]]
+    else:
+        col = random.randint(0, 1)
+        image[:, col] = colors[chosen_colors[0]]
+
+    # Second selection
+    if start_with_row:
+        col = random.randint(0, 1)
+        image[:, col] = colors[chosen_colors[1]]
+    else:
+        row = random.randint(0, 1)
+        image[row, :] = colors[chosen_colors[1]]
+
+    # Third selection
+    if start_with_row:
+        while True:
+            new_row = random.randint(0, 1)
+            if new_row != row:
+                break
+        image[new_row, :] = colors[chosen_colors[2]]
+    else:
+        while True:
+            new_col = random.randint(0, 1)
+            if new_col != col:
+                break
+        image[:, new_col] = colors[chosen_colors[2]]
+
+    # Fourth selection
+    if start_with_row:
+        while True:
+            new_col = random.randint(0, 1)
+            if new_col != col:
+                break
+        image[:, new_col] = colors[chosen_colors[3]]
+    else:
+        while True:
+            new_row = random.randint(0, 1)
+            if new_row != row:
+                break
+        image[new_row, :] = colors[chosen_colors[3]]
+
+    return (image, dangerous)
+
 def _generate_diagram():
     # Initialize a 20x20 array (0 represents white color)
     image = np.zeros((20, 20), dtype=int)
@@ -81,6 +239,82 @@ def _generate_diagram():
     # Define colors mapping to integers
     colors = {
         "Red": 1,
+        "Green": 2,
+        "Blue":3,
+        "Yellow": 4
+    }
+
+    # Randomly choose whether to start with rows or columns
+    start_with_row = random.choice([True, False])
+
+    # Choose four different colors
+    chosen_colors = random.sample(list(colors.keys()), 4)
+
+    red_ind = -1
+    yellow_ind = -1
+    dangerous = False
+    for i, _ in enumerate(chosen_colors):
+        if (chosen_colors[i] == "Red"):
+            red_ind = i
+        if (chosen_colors[i] == "Yellow"):
+            yellow_ind = i
+    if red_ind < yellow_ind:
+        dangerous = True
+
+    # First selection
+    if start_with_row:
+        row = random.randint(0, 19)
+        image[row, :] = colors[chosen_colors[0]]
+    else:
+        col = random.randint(0, 19)
+        image[:, col] = colors[chosen_colors[0]]
+
+    # Second selection
+    if start_with_row:
+        col = random.randint(0, 19)
+        image[:, col] = colors[chosen_colors[1]]
+    else:
+        row = random.randint(0, 19)
+        image[row, :] = colors[chosen_colors[1]]
+
+    # Third selection
+    if start_with_row:
+        while True:
+            new_row = random.randint(0, 19)
+            if new_row != row:
+                break
+        image[new_row, :] = colors[chosen_colors[2]]
+    else:
+        while True:
+            new_col = random.randint(0, 19)
+            if new_col != col:
+                break
+        image[:, new_col] = colors[chosen_colors[2]]
+
+    # Fourth selection
+    if start_with_row:
+        while True:
+            new_col = random.randint(0, 19)
+            if new_col != col:
+                break
+        image[:, new_col] = colors[chosen_colors[3]]
+    else:
+        while True:
+            new_row = random.randint(0, 19)
+            if new_row != row:
+                break
+        image[new_row, :] = colors[chosen_colors[3]]
+
+    return (image, dangerous)
+
+
+def _generate_diagram_weighted():
+    # Initialize a 20x20 array (0 represents white color)
+    image = np.zeros((20, 20), dtype=int)
+
+    # Define colors mapping to integers
+    colors = {
+        "Red": 4 ,
         "Green": 2,
         "Blue":3,
         "Yellow": 4
@@ -169,5 +403,5 @@ def print_diagram(image):
 
 
 if __name__ == "__main__":
-    diagram = _generate_diagram()
-    print_diagram(diagram)
+    
+    print(convert_diagram_hot((np.array([[1]]), np.array([False]))))
--- sanity.py
+++ sanity.py
@@ -1,116 +1,15 @@
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.model_selection import train_test_split
-from image import generate_diagram
-
-# # Load the iris dataset
-# data = generate_diagram(1000)
-# X = data[0]
-# y = data[1]
-
-
-# # Split the data into training and test sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# # Create and train the model
-# model = LogisticRegression()
-# model.fit(X_train, y_train)
-
-# # Make predictions on the test set
-# predictions = model.predict(X_test)
-# # Compute the accuracy
-# num_correct = 0
-# for i, _ in enumerate(predictions):
-#     if predictions[i] == y_test[i]:
-#         num_correct += 1
-
-# print("Accuracy on test data:", num_correct / len(predictions))
-
 import numpy as np
+import sklearn.manifold as manifold
 
-# def generate_data(num_samples):
-#     # Implement your data generation logic here
-#     X = np.random.rand(num_samples, 20, 20, 4)  # Example random data
-#     Y = np.random.randint(0, 2, num_samples)   # Binary labels: 0 for 'Safe', 1 for 'Dangerous'
-#     return X.reshape(num_samples, -1), Y       # Reshape X to be 2D
+from image import *
+import matplotlib.pyplot as plt
 
-X_train, Y_train = generate_diagram(400)
-X_test, Y_test = generate_diagram(200)
+X, y = generate_diagram_nonlinear1(2500)
 
-
-input_size = 20 * 20  # Flatten the 20x20x4 input [0,0,0,0], blue: [1,0,0,0], green: [0,1,0,0]
-hidden_size = 128         # Size of the hidden layer
-output_size = 1           # One neuron for binary output
-
-# Initialize weights and biases
-W1 = np.random.randn(input_size, hidden_size) * 0.01
-b1 = np.zeros((1, hidden_size))
-W2 = np.random.randn(hidden_size, output_size) * 0.01
-b2 = np.zeros((1, output_size))
-
-
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
-
-
-
-def forward_prop(X):
-    Z1 = np.dot(X, W1) + b1
-    A1 = sigmoid(Z1)
-    Z2 = np.dot(A1, W2) + b2
-    A2 = sigmoid(Z2)
-    return A1, A2
-
-
-def compute_loss(Y, A2):
-    m = Y.shape[0]
-    log_loss = -np.sum(Y * np.log(A2) + (1 - Y) * np.log(1 - A2))
-    return log_loss / m
-
-
-
-def back_prop(X, Y, A1, A2):
-    m = X.shape[0]
-    dZ2 = A2 - Y
-    dW2 = np.dot(A1.T, dZ2) / m
-    db2 = np.sum(dZ2, axis=0, keepdims=True) / m
-    dZ1 = np.dot(dZ2, W2.T) * (A1 * (1 - A1))
-    dW1 = np.dot(X.T, dZ1) / m
-    db1 = np.sum(dZ1, axis=0, keepdims=True) / m
-    return dW1, db1, dW2, db2
-
-
-
-def update_params(dW1, db1, dW2, db2, learning_rate):
-    global W1, b1, W2, b2
-    W1 -= learning_rate * dW1
-    b1 -= learning_rate * db1
-    W2 -= learning_rate * dW2
-    b2 -= learning_rate * db2
-
-
-num_epochs = 1000
-learning_rate = 0.01
-
-for epoch in range(num_epochs):
-    A1, A2 = forward_prop(X_train)
-    cost = compute_loss(Y_train, A2)
-    dW1, db1, dW2, db2 = back_prop(X_train, Y_train, A1, A2)
-    update_params(dW1, db1, dW2, db2, learning_rate)
-
-    if epoch % 100 == 0:
-        print(f"Epoch {epoch}, loss: {cost}")
-
-
-#Make predictions
-
-def predict(X):
-    _, A2 = forward_prop(X)
-    predictions = A2 > 0.5
-    return predictions
-
-# Predict on test data
-test_predictions = predict(X_test)
-test_accuracy = np.mean(test_predictions == Y_test)
-print(f"Test accuracy: {test_accuracy}")
-
-
+# plot tnse
+for perp in [1,2,3,4,5,6,7,8,9,10,12,15,17,20,25,30,35,40,45,50,60,70,80,90,100]:
+    X_embedded = manifold.TSNE(n_components=2, perplexity=perp).fit_transform(X.copy())
+    plt.scatter(X_embedded[:,0], X_embedded[:,1], c=y)
+    plt.savefig(f"Timages/tnse{perp}.png")
+    plt.clf()
+    print("Progress:", perp)
